ec96f18d805ed74b31cc7a14be8dbb13d80ece99
JGomezj <john.gomez1.mt@correo.usa.edu.co>
2021-11-18 09:16:12 -0500

    commit de archivos para publicar reto 2 mt

---
+++ js/funciones_clientes.js
@@ -0,0 +1,405 @@
+var urlAPIMsg = "https://gb4896c6dd4008f-bdgastos.adb.ca-montreal-1.oraclecloudapps.com/ords/admin/message/message"; 
+var urlAPI = "https://gb4896c6dd4008f-bdgastos.adb.ca-montreal-1.oraclecloudapps.com/ords/admin/client/client";
+
+var countRow = 0;
+var countRowMsg = 0;
+
+function getData(page, urlActual){    
+    
+    $.ajax({
+        url: urlActual, 
+        datatype: 'JSON',
+        type: 'GET',
+        success: function(data){
+            console.log(data);
+            if(page =='Clientes'){
+                upDatatable(data.items);
+            }
+            else{
+                upDatatableMsg(data.items);
+            }        
+            console.log(countRow)    
+        }, 
+        error: function(XHR, status, error){
+            alert("Error: " + error)
+        }
+    });
+}
+
+function getDatabyId(campoId, page, urlActual){
+    var id = campoId.val();
+    if(id == ""){
+        alert('Campo ID no puede estar vacio !!!');
+        return;
+    }
+
+    $.ajax(
+        {
+            url: urlActual + '/' + id,
+            dataType: 'JSON',
+            type: 'GET',
+            success: function(data){
+                if(data.items.length == 0){
+                    alert("Campo ID no encontrado")
+                    if(page=="Clientes"){
+                        $('#consultaID').val("");
+                    }
+                    else{
+                        $('#consultaIDMsg').val("");
+                    }
+                    getData(page, urlActual);
+                    return
+                }
+
+                // $('#listClientes > tbody').empty();
+                if(page =='Clientes'){
+                    upDatatable(data.items);
+                }
+                else{
+                    upDatatableMsg(data.items);
+                }
+            },
+            error: function(xhr, status, error){
+                alert('ha sucedido un error, ' + xhr.status + ' ' + error);
+                // $('#consultaID').val("");
+                if(page=="Clientes"){
+                    $('#consultaID').val("");
+                }
+                else{
+                    $('#consultaIDMsg').val("");
+                }
+            }
+        }
+    );
+}
+
+function insertDataClient(){ 
+    console.log('insertDataClient');
+    let name = $('#nombre_modal').val();
+    let email = $('#email_modal').val();
+    let age = parseInt($('#edad_modal').val());
+
+    $.ajax({
+        type: "POST",
+        url: urlAPI,
+        dataType: "json",
+        data: {
+            'id': countRow+1,
+            'name' :name, 
+            'email' : email,
+            'age' : age
+        },
+        success: function(info){
+            console.log('Registro guardado !!!')
+        }
+    });
+}
+
+function insertDataMsg(){ 
+    console.log('insertDataMsg');
+    let msg = $('#msg_modal').val();
+    
+    $.ajax({
+        type: "POST",
+        url: urlAPIMsg,
+        dataType: "json",
+        data: {
+            'id': countRowMsg+1,
+            'messagetext' :msg
+        },
+        success: function(info){
+            console.log('Registro guardado !!!')
+        }
+    });
+}
+
+function updateDataClient(){
+    console.log('updateDataClient');
+    let MyData = {
+        id : $("#id_modal").val(),
+        name : $("#nombre_modal").val(),
+        email : $("#email_modal").val(),
+        age : $("#edad_modal").val()
+    };
+
+    console.log(MyData);
+    let dataToSend = JSON.stringify(MyData);
+
+    $.ajax({
+        url: urlAPI, 
+        type: "PUT",
+        data:dataToSend,
+        contentType:"application/JSON",
+        dataType: "JSON",
+        error: function (xhr, status) {
+            console.log('ha sucedido un problema, ' + xhr.status);
+        },
+            complete: function (xhr, status) {
+            console.log('Petición realizada, ' + xhr.status);
+        },
+        success: function(data){
+            console.log(`Registro guardado !!!`);
+        }        
+    });
+}
+
+function updateDataMsg(){
+    console.log('updateDataMsg');
+    let MyData = {
+        id : $("#idmsg_modal").val(),
+        messagetext : $("#msg_modal").val()
+    };
+
+    console.log(MyData);
+    let dataToSend = JSON.stringify(MyData);
+
+    $.ajax({
+        url: urlAPIMsg, 
+        type: "PUT",
+        data:dataToSend,
+        contentType:"application/JSON",
+        dataType: "JSON",
+        error: function (xhr, status) {
+            console.log('ha sucedido un problema, ' + xhr.status);
+        },
+            complete: function (xhr, status) {
+            console.log('Petición realizada, ' + xhr.status);
+        },
+        success: function(data){
+            console.log(`Registro guardado !!!`);
+        }        
+    });
+}
+
+function deleteData(idElement, page, urlActual){
+    console.log('deleteData');
+    let MyData = {
+        id: parseInt(idElement)
+    };
+
+    let dataToSendDelete=JSON.stringify(MyData);
+    $.ajax({
+        url: urlActual, //"https://gb4896c6dd4008f-bdgastos.adb.ca-montreal-1.oraclecloudapps.com/ords/admin/client/client",
+        type:"DELETE",        
+        data: dataToSendDelete,
+        contentType:"application/JSON",
+        dataType: "JSON",
+        error: function (xhr, status) {
+            console.log('ha sucedido un problema, ' + xhr.status);
+        },
+        complete: function (xhr, status) {
+            console.log('Petición realizada, ' + xhr.status);
+        },success : function(info){
+            console.log(`registro eliminado !!!`);
+            getData(page, urlActual);
+        }
+    });
+}
+
+function upDatatable(items){
+    //countRow = 0;
+    var lstClientes = "";
+    var lastId = 0;
+    for (let i = 0; i < items.length; i++) { 
+        lastId = items[i].id;
+
+        lstClientes += "<tr>"
+        lstClientes += "<td id='id_item' class='dataCenter ajustarCol'>" + items[i].id 
+        lstClientes += "<td>" + items[i].name
+        lstClientes += '<td class="dataCenter">' + items[i].email
+        lstClientes += "<td class='dataRight'>" + items[i].age
+        lstClientes += "<td class='ajustarCol'><center><button id='btn_nuevoReg' type='button' class='btn btn-danger' onclick='eliminarReg()'><i class='far fa-minus-square'></i></button>"
+        lstClientes += "<td class='ajustarCol'><center><button id='btn_editarReg' type='button' class='btn btn-warning' data-bs-toggle='modal' data-bs-target='#exampleModal' onclick='cargarInfoModal()'><i class='far fa-edit'></i></button>"
+    }
+    $('#listClientes').append(lstClientes);
+    countRow = lastId;
+}
+
+function upDatatableMsg(items){
+    //countRow = 0;
+    var lstMsg = "";
+    var lastId = 0;
+    for (let i = 0; i < items.length; i++) { 
+        lastId = items[i].id;
+
+        lstMsg += "<tr>"
+        lstMsg += "<td id='id_item' class='dataCenter ajustarCol'>" + items[i].id 
+        lstMsg += "<td>" + items[i].messagetext
+        lstMsg += "<td class='ajustarCol'><center><button id='btn_nuevoRegMsg' type='button' class='btn btn-danger' onclick='eliminarRegMsg()'><i class='far fa-minus-square'></i></button>"
+        lstMsg += "<td class='ajustarCol'><center><button id='btn_editarRegMsg' type='button' class='btn btn-warning' data-bs-toggle='modal' data-bs-target='#exampleModalMsg' onclick='cargarInfoModalMsg()'><i class='far fa-edit'></i></button>"
+    }
+    $('#listMensajes').append(lstMsg);
+    countRowMsg = lastId;
+}
+
+function ConvertirStringToDate(fechaString)
+{
+    var fechasplit = fechaString.split('-');
+    let dd = fechasplit[2];
+    let mm = fechasplit[1];
+    let aaaa = fechasplit[0];
+    let fecha_corregida = `${dd}-${mm}-${aaaa}`;
+       
+    return fecha_corregida;
+}
+
+function ConvertirStringToDate2(fechaString)
+{
+    var fechasplit = fechaString.split('/');
+    let dd = fechasplit[2];
+    let mm = fechasplit[1];
+    let aaaa = fechasplit[0];
+    let fecha_corregida = `${dd}-${mm}-${aaaa}`;
+       
+    return fecha_corregida;
+}
+
+/* Funciones llamadas desde el form Clientes */
+function consultarCliente(campoId){
+    $('#listClientes > tbody').empty();
+    getDatabyId(campoId, "Clientes", urlAPI);
+}
+
+function cargarDatos(){
+    $('#consultaID').val('');
+    $('#listClientes > tbody').empty();
+    getData("Clientes", urlAPI);
+}
+
+function validarEmail(){
+    if($("#email_modal").val().indexOf('@',0) == -1 || $("#email_modal").val().indexOf('.', 0) == -1){
+        alert('El correo electrónico introducido no es correcto.');
+            return false;
+    }
+    return true;
+}
+
+function nuevoCliente(){
+    if(!validarEmail())
+        return;
+    
+    $('#listClientes > tbody').empty();
+    insertDataClient();
+    getData("Clientes", urlAPI);
+    limpiarControles();
+    $('#exampleModal').modal('toggle');
+}
+
+function cargarInfoModal(){
+    $('#nuevoRegC').hide();
+    $('#btn_NewRec').hide();
+
+    $('#editarRegC').show();
+    $('#btn_EditRec').show();
+    $('#id_modal_div').show();
+    $('#id_modal').prop( 'disabled', true);
+
+    $('#listClientes tbody tr').click(function () {
+        $('#id_modal').val($(this).find('td:first-child').html()),
+        $('#nombre_modal').val($(this).find('td:nth-child(2)').html()),
+        $('#email_modal').val($(this).find('td:nth-child(3)').html()),
+        $('#edad_modal').val($(this).find('td:nth-child(4)').html())
+    });
+}
+
+function editarCliente(){
+    if(!validarEmail()){
+        return;
+    }
+
+    $('#listClientes > tbody').empty();
+    updateDataClient();
+    getData("Clientes", urlAPI);
+    limpiarControles();
+    $('#exampleModal').modal('toggle');
+}
+
+function eliminarReg(){     
+    if(confirm("desea eliminar el registro seleccionado?"))
+    {
+        $('#listClientes tbody tr').click(function () { 
+            var deleteid = $(this).find('td:first-child').html();
+            deleteData(parseInt($(this).find('td:first-child').html()),"Clientes", urlAPI);
+            $('#listClientes > tbody').empty();
+        }); 
+    }   
+}
+
+function viewContentModalNew(){
+    $('#nuevoRegC').show();
+    $('#btn_NewRec').show();
+
+    $('#editarRegC').hide();
+    $('#btn_EditRec').hide();
+    $('#id_modal_div').hide();
+    limpiarControles();
+}
+
+function limpiarControles(){
+    $('#nombre_modal').val(''),
+    $('#email_modal').val(''),
+    $('#edad_modal').val('')
+}
+
+/* Funciones llamadas desde el form Mensajes */
+function consultarMsg(campoId){
+    $('#listMensajes > tbody').empty();
+    getDatabyId(campoId, "Mensajes", urlAPIMsg);
+}
+
+function cargarDatosMsg(){
+    $('#consultaIDMsg').val('');
+    $('#listMensajes > tbody').empty();
+    getData("Mensajes", urlAPIMsg);
+}
+
+function nuevoMsg(){
+    $('#listMensajes > tbody').empty();
+    insertDataMsg();
+    $('#msg_modal').val('')
+    $('#exampleModalMsg').modal('toggle');
+    getData("Mensajes", urlAPIMsg);
+}
+
+function cargarInfoModalMsg(){
+    $('#nuevoRegMsg').hide();
+    $('#btn_NewRecMsg').hide();
+
+    $('#editarRegMsg').show();
+    $('#btn_EditRecMsg').show();
+    $('#idMsg_modal_div').show();
+    $('#idmsg_modal').prop('disabled', true);
+
+    $('#listMensajes tbody tr').click(function () {
+        $('#idmsg_modal').val($(this).find('td:first-child').html()),
+        $('#msg_modal').val($(this).find('td:nth-child(2)').html())
+    });
+}
+
+function editarMsg(){
+    $('#listMensajes > tbody').empty();
+    updateDataMsg();
+    getData("Mensajes", urlAPIMsg);
+    $('#msg_modal').val('');
+    $('#exampleModalMsg').modal('toggle');
+}
+
+function eliminarRegMsg(){     
+    if(confirm("desea eliminar el registro seleccionado?"))
+    {
+        $('#listMensajes tbody tr').click(function () { 
+            var deleteid = $(this).find('td:first-child').html();
+            deleteData(parseInt($(this).find('td:first-child').html()), "Mensajes", urlAPIMsg);
+            $('#listMensajes > tbody').empty();
+        }); 
+    }   
+}
+
+function viewContentModalNewMsg(){
+    $('#nuevoRegMsg').show();
+    $('#btn_NewRecMsg').show();
+
+    $('#editarRegMsg').hide();
+    $('#btn_EditRecMsg').hide();
+    $('#idMsg_modal_div').hide();
+}
---
+++ js/script.js
@@ -0,0 +1,189 @@
+// declaramos variables
+var side_menu, btn_open, body, header_page, main_content, footer_page, 
+container_footer, nuevo_registro, page_clientes, page_mensajes;
+
+var urlAPIMsg = "https://gb4896c6dd4008f-bdgastos.adb.ca-montreal-1.oraclecloudapps.com/ords/admin/message/message"; 
+var urlAPI = "https://gb4896c6dd4008f-bdgastos.adb.ca-montreal-1.oraclecloudapps.com/ords/admin/client/client";
+
+$(document).ready(function(){    
+    agregarMenu();
+    getData("Clientes", urlAPI);
+    getData("Mensajes", urlAPIMsg);
+    agregarFooter();
+    
+
+    $('body').on('click', '#side_menu a', function(e){
+        // $("a").addClass('selected');
+        console.log(page_actual);
+        
+        var page_actual = $(this).attr('id');
+        if(page_actual==page_clientes){
+            console.log(page_actual);
+            $(page_actual).addClass('selected');
+        }
+        //e.preventDefault();
+    })
+
+    var sm = $('#side_menu');
+    var btn_op = $('#btn_open');
+    var b = $('#body')
+    var hp = $('#header_page')
+    var m = $('#main_content')
+    var nr = $('#btn_nuevoReg')
+    var cfp = $('#container_footer')
+    var pc = $('page_Clientes')
+    var pm = $('page_Mensajes')
+
+    side_menu = sm; 
+    body = b;
+    header_page = hp;
+    main_content = m;
+    nuevo_registro = nr;
+    container_footer = cfp;
+    page_clientes = pc;
+    page_mensajes = pm;
+    
+});
+// class="selected"
+
+function agregarMenu(){
+    var menu = $('#menu');
+    menu.html('<header id="header_page">'+
+            '<div class="icon_menu">'+
+                '<i id="btn_open" class="fas fa-bars" onclick="openMenu()"></i>'+
+            '</div>'+
+        '</header>'+
+        '<div id="side_menu" class="menu_side">'+
+            '<div class="name_page">'+
+                '<i class="fa fa-youtube"></i>'+
+                '<h4>Reto2G43MT</h4>'+
+            '</div>'+
+            '<div class="options_menu">'+
+                '<a id="page_Clientes" href="index.html">'+
+                    '<div class="option">'+
+                        '<i class="fas fa-home" title="Clientes"></i>'+
+                        '<h4>Clientes</h4>'+
+                    '</div>'+
+                '</a>'+
+                '<a id="page_Mensajes" href="mensajes.html">'+
+                    '<div class="option">'+
+                        '<i class="fas fa-file" title="Mensajes"></i>'+
+                        '<h4>Mensajes</h4>'+
+                    '</div>'+
+                '</a>'+
+                '<a href="#" hidden>'+
+                    '<div class="option">'+
+                        '<i class="fas fa-video" title="Cursos"></i>'+
+                        '<h4>Cursos</h4>'+
+                    '</div>'+
+                '</a>'+
+                '<a href="#" hidden>'+
+                    '<div class="option">'+
+                        '<i class="fas fa-sticky-note" title="Blog"></i>'+
+                        '<h4>Blog</h4>'+
+                    '</div>'+
+                '</a>'+
+                '<a href="#" hidden>'+
+                    '<div class="option">'+
+                        '<i class="fas fa-id-badge" title="Contacto"></i>'+
+                        '<h4>Contacto</h4>'+
+                    '</div>'+
+                '</a>'+
+                '<a href="#" hidden>'+
+                    '<div class="option">'+
+                        '<i class="fas fa-address-card" title="Nosotros"></i>'+
+                        '<h4>Nosotros</h4>'+
+                    '</div>'+
+                '</a>'+
+            '</div>'+
+        '</div>'
+    );
+}
+
+function agregarFooter(){
+    var footer = $('#footer');
+    footer.html(//'<footer>' + 
+        '<div class="container_footer">' +
+            '<div class="box_footer">' +
+                '<div class="logo">' +
+                    '<img src="https://misiontic2022.usergioarboleda.edu.co/assets/images/banner_mintic_logo.svg" alt="MisionTic2021">' +
+                '</div>' +
+                '<div class="terms">' +
+                    '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo tenetur obcaecati omnis molestiae corporis sequi delectus modi nemo animi dolorem nam perspiciatis totam veniam blanditiis doloribus ipsa, temporibus nihil. Obcaecati?</p>' +
+                '</div>' +
+            '</div>' +
+            '<div class="box_footer">' +
+                '<h2>Soluciones</h2>' +
+                '<a href="">App Desarrollo</a>' +
+                '<a href="">App Marketing</a>' +
+                '<a href="">IOS Desarrollo</a>' +
+                '<a href="">Android Desarrollo</a>' +
+            '</div>' +
+            '<div class="box_footer">' +
+                '<h2>Compañia</h2>' +
+                '<a href="">Acerca de</a>' +
+                '<a href="">Trabajos</a>' +
+                '<a href="">Procesos</a>' +
+                '<a href="">Servicios</a>' +
+            '</div>' +
+            '<div class="box_footer">' +
+                '<h2>Redes Sociales</h2>' +
+                '<a href="#"><i class="fab fa-facebook-square"></i> Facebook</a>' +
+                '<a href="#"><i class="fab fa-twitter-square"></i> Twitter</a>' +
+                '<a href="#"><i class="fab fa-linkedin"></i> Linkedin</a>' +
+                '<a href="#"><i class="fab fa-instagram-square"></i> Instagram</a>' +
+            '</div>' +
+        '</div>' +
+        '<div class="box_copyrigth">' +
+            '<hr>' +
+            '<p>Todos los derechos reservados. &copy; 2021 <b>by JGomezJ</b></p>' +
+        '</div>' //+ '</footer>'
+    );
+}
+
+function openMenu(){
+    body.toggleClass('body_move');
+    side_menu.toggleClass('menu_side_move');
+    header_page.toggleClass('header_page_move');
+    main_content.toggleClass('main_content_move');
+    //footer_page.toggleClass('footer_move');
+    nuevo_registro.toggleClass('nuevo_registro_move');
+}
+// evento para mostrar y ocultar menu
+// ejecutar funcion en el evento click
+
+if(window.innerWidth < 760){
+    body.toggleClass('body_move');
+    side_menu.toggleClass('menu_side_move');
+    header_page.toggleClass('header_page_move');
+    main_content.toggleClass('main_content_move');
+    footer_page.toggleClass('footer_move')
+    nuevo_registro.toggleClass('nuevo_registro_move')
+}
+
+// menu responsive
+$(document).on('resize', function(){
+    if(window.innerWidth > 760){
+        body.removeClass('body_move');
+        side_menu.removeClass('menu_side_move');
+        header_page.removeClass('header_page_move');
+        main_content.removeClass('main_content_move');
+        footer_page.removeClass('footer_move')
+        nuevo_registro.removeClass('nuevo_registro_move')
+    }
+
+    if(window.innerWidth < 760){
+        body.addClass('body_move');
+        side_menu.addClass('menu_side_move');
+        header_page.addClass('header_page_move');
+        main_content.addClass('main_content_move');
+        footer_page.addClass('footer_move')
+        nuevo_registro.addClass('nuevo_registro_move')
+    }
+});
+
+
+// https://www.youtube.com/watch?v=idRdAgNnC5Q&list=RDCMUCgY6raSf9MhsMjXwmG83-Bg&start_radio=1&rv=idRdAgNnC5Q&t=16324
+// https://code.tutsplus.com/es/tutorials/making-a-sliding-side-navigation-menu-for-responsive-designs--cms-28400
+// https://codepen.io/AlberPedraza/pen/PbKeVb
+// https://stackoverflow.com/questions/31789294/classlist-toggle-not-working
